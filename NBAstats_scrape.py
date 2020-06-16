c8d8778ad31ae5e626327fbf44801590eba8364f
William Zhang <williamyixin@berkeley.edu>
2020-06-15 19:34:52 -0700

    Finished scraper
    Roses are red
    I finshed scraping
    Fixed all the bugs
    now do your pandas thing

--- NBAstats_scrape.py
+++ NBAstats_scrape.py
@@ -4,25 +4,44 @@ import csv
 
 # TODO: ADD THE CURRENT DATE REQUESTED AS A SUFFIX TO THE FILE NAME
 player_data = "Player_Data.csv"
-player_data_headers = ['Name', 'Team', 'PER', 'Minutes Played']
+player_data_headers = ['Name', 'Team', 'Minutes Played', 'PER']
 
-with open(player_data, 'wb+') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames = player_data_headers, delimiter = ',')
-    writer.writeheader()
+with open(player_data, mode = 'w') as csv_file:
     writer = csv.writer(csv_file)
+    writer.writerow(player_data_headers)
+    
+
     url = 'https://www.basketball-reference.com/leagues/NBA_2020_advanced.html'
     r = requests.get(url)
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
 
     table = soup.find('tbody')
-
+    all_rows = []
     for entry in table.find_all('tr'):
         row = []
         for cell in entry.find_all('td'):
             if cell.attrs['data-stat'] == 'player':
-                name = cell.find('a').text.strip()
-                print(name)
+                name = cell.find('a').text.strip().encode('utf-8')
+                row.append(name)
+            elif cell.attrs['data-stat'] == 'team_id':
+                team = ""
+                if cell.find('a') == None: 
+                    team = cell.text.strip().encode('utf-8')
+                else: 
+                    team = cell.find('a').text.strip().encode('utf-8')
+                row.append(team)
+            elif cell.attrs['data-stat'] == 'mp':
+                mp = cell.text.strip().encode('utf-8')
+                row.append(mp)
+            elif cell.attrs['data-stat'] == 'per':
+                per = cell.text.strip().encode('utf-8')
+                row.append(per)
+        all_rows.append(row)
+
+    print(all_rows)
+    writer.writerows(all_rows)
+        
 
 
 
