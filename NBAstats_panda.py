18cf29dcfd99a7ea058c5ed684ce032b067fb2ff
Timothy Wu <wutimoth@usc.edu>
2020-06-16 18:05:29 -0700

    Hello again
    hello again

--- NBAstats_panda.py
+++ NBAstats_panda.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
 
-data = pd.read_csv("Player_Data.csv", encoding='utf-8')
-data = data.astype("string")
-data.apply(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
-#for col in data.columns:
- #   data[col] = data[col].str.slice(2, -1)
-
-#data['Minutes Played'] = pd.to_numeric(data['Minutes Played'])
-#data['PER'] = pd.to_numeric(data['PER'])
+data = pd.read_csv("Player_Data_pandas.csv", index_col=[1,2], encoding='utf-8')
+data.dropna(axis=0, how='all',thresh=None,subset=None,inplace=True)
 print(data)
-print(data.dtypes)
