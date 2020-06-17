c3b6fd337fefba25d897601e30cd32dfd63f25eb
Timothy Wu <wutimoth@usc.edu>
2020-06-16 22:20:03 -0700

    PER_Calculator Commit #1
    Roses are Red
    PER is cancer
    But hopefully all this work
    Will be a model enhancer

--- NBAstats_panda.py
+++ NBAstats_panda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 
-data = pd.read_csv("Player_Data_pandas.csv", index_col=[1,2], encoding='utf-8')
+data = pd.read_csv("Player_Data_pandas.csv", index_col=[0,1], encoding='utf-8')
 data.dropna(axis=0, how='all',thresh=None,subset=None,inplace=True)
 print(data)
--- PER_Calculator.py
+++ PER_Calculator.py
@@ -1,49 +1,56 @@
-minutes_played = 1564
-three_point = 1
-assist = 141
-factor = (2/3) - ((0.5 * league_assists/league_field_goals)
-    / (2 * league_field_goals/league_free_throws))
-team_assist = 1400
-team_field_goal = 5447
-field_goal = 262
-free_throw = 108
-value_of_possession = (league_points / 
-    (league_field_goals_attempted - league_offensive_rebound +
-    league_turnover + 0.44 * league_free_throws_attempted))
-turnover = 86
-defensive_rebound_percentage = (league_total_rebound - league_offensive_rebound)/league_total_rebound
-field_goal_attempted = 443
-free_throw_attempted = 183
-total_rebound = 543
-offensive_rebound = 196
-steal = 50
-blocks = 65
-personal_fouls = 111
-league_free_throws
-league_personal_fouls
-league_free_throws_attempted
-league_assists
-league_field_goals
-league_points
-league_field_goals_attempted
-league_offensive_rebound
-league_turnover
-league_total_rebound
 
+# function that calculates a given player's adjusted PER
+def aPER_Calculator(MP, threeP, AST, lg_AST, lg_FG, lg_FT, team_AST, team_FG, FG, FTA, FT, lg_PTS, lg_FGA, lg_ORB, lg_TOV, lg_FTA, TOV, lg_TRB, FGA, TRB, ORB, STL, BLK, PF, lg_PF, lg_Pace, team_Pace):
+    # calculate factor
+    Factor = Factor_Calculator(lg_AST, lg_FG, lg_FT)
+    
+    # calculate value of possession
+    VOP = VOP_Calculator(lg_PTS, lg_FGA, lg_ORB, lg_TOV, lg_FTA)
+    
+    # calculate defensive rebound percentage 
+    DRB = DRB_Calculator(lg_TRB, lg_ORB)
+    
+    # calculate pace adjustment
+    pace = Pace_Adjustment(lg_Pace, team_Pace)
+    
+    # main formula for unadjusted PER
+    uPER = ((1 / MP) * 
+        [threeP 
+        + (2 / 3) * AST 
+        + (2 - Factor * (team_AST / team_FG)) * FG 
+        + (FT * 0.5 * (1 + (1 - (team_AST / team_FG)) 
+        + (2 / 3) * (team_AST / team_FG))) 
+        - VOP *TOV 
+        - VOP * DRB * (FGA - FG) 
+        - VOP * 0.44 * (0.44 + (0.56 * DRB)) * (FTA - FT) 
+        + VOP * (1 - DRB) * (TRB - ORB) 
+        + VOP * DRB * ORB 
+        + VOP * STL 
+        + VOP * DRB * BLK 
+        - PF * ((lg_FT / lg_PF) - 0.44 * (lg_FTA / lg_PF) * VOP)])
 
-PER = (1/minutes_played) * (three_point - 
-    (personal_fouls * league_free_throws/league_personal_fouls) + 
-    (free_throw/2) * (2 - team_assist/(3 * team_field_goal)) + 
-    (field_goal * (2 - (factor * team_assist/team_field_goal))) +
-    (2 * assist/3) + value_of_possession * (defensive_rebound_percentage * 
-    (2 * offensive_rebound + blocks - 0.2464 * 
-    (free_throw_attempted - free_throw) - (field_goal_attempted - field_goal) - total_rebound)
-    + (0.44 * league_free_throws_attempted * personal_fouls/league_personal_fouls) 
-    - (turnover + offensive_rebound) + steal + total_rebound - 0.1936
-    (free_throw_attempted - free_throw)))
+    # return adjusted PER
+    return (pace* uPER)
 
+# function that calculates the factor part of the PER equation
+def Factor_Calculator(lg_AST, lg_FG, lg_FT):
+    return ((2 / 3) - (0.5 * (lg_AST / lg_FG)) / (2 * (lg_FG / lg_FT)))
 
+# function that calculates the value of possession part of the PER equation
+def VOP_Calculator(lg_PTS, lg_FGA, lg_ORB, lg_TOV, lg_FTA):
+    return (lg_PTS / (lg_FGA - lg_ORB + lg_TOV + 0.44 * lg_FTA))
 
+# function that calculates the defensive rebound percentage part of the PER equation
+def DRB_Calculator(lg_TRB, lg_ORB):
+    return ((lg_TRB - lg_ORB) / lg_TRB)
 
+# function that calculates the pace adjustment part of the PER equation
+def Pace_Adjustment(lg_Pace, team_Pace):
+    return (lg_Pace / team_Pace)
 
-
+# takes a list of adjusted PERs and standardizes them so that the average is 15
+def PER_Calculator(aPERs):
+    lg_aPER = aPERs.mean()
+    for elem in aPERs:
+        elem = elem * (15 / lg_aPER)
+    return aPERs
