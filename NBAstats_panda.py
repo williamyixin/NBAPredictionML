092e7e5b8b81ffa722870fe9429e266d6cae02f3
Timothy Wu <wutimoth@usc.edu>
2020-06-16 17:14:26 -0700

    hello
    hello

--- NBAstats_panda.py
+++ NBAstats_panda.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 
-data = pd.read_csv("Player_Data.csv")
-print(data.head())
+data = pd.read_csv("Player_Data.csv", encoding='utf-8')
+for col in data.columns:
+    data[col] = data[col].str.decode('utf-8')
+print(data)
+print(data.dtypes)
