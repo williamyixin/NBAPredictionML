7210b76552b85207343d79aeb326ef22f221b916
Timothy Wu <wutimoth@usc.edu>
2020-08-06 00:34:21 -0700

    sending you the sauce

--- test_data_generator.py
+++ test_data_generator.py
@@ -9,7 +9,7 @@ year = 2020
 player_stats = ['player', 'mp', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA', 'ORB','DRB','AST','STL','BLK','TOV','PF','PTS']
 
 def get_input():
-    f = open('input/HOU_VS_POR.in', "r",encoding='utf-8')
+    f = open('input/BRK_VS_BOS.in', "r",encoding='utf-8')
     away_starter = []
     away_other = []
     home_starter = []
